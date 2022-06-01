75e85a562be5774bc899eee0c9a991c3487d1c34
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-01 22:00:53 +0100

    Category

---
+++ src/main/java/com/myprojectweb/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.myprojectweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myprojectweb.course.entities.Category;
+import com.myprojectweb.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
