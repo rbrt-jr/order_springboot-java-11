8df21d7b9f9c7051a52f63408285598340932ad9
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-05 21:01:58 +0100

    Product

--- src/main/java/com/myprojectweb/course/config/TestConfig.java
+++ src/main/java/com/myprojectweb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.myprojectweb.course.entities.Category;
 import com.myprojectweb.course.entities.Order;
+import com.myprojectweb.course.entities.Product;
 import com.myprojectweb.course.entities.User;
 import com.myprojectweb.course.entities.enums.OrderStatus;
 import com.myprojectweb.course.repositories.CategoryRepository;
 import com.myprojectweb.course.repositories.OrderRepository;
+import com.myprojectweb.course.repositories.ProductRepository;
 import com.myprojectweb.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -45,6 +50,13 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 	}
 }
---
+++ src/main/java/com/myprojectweb/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.myprojectweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myprojectweb.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
