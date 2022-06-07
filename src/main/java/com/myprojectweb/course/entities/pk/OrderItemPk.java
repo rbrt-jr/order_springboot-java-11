cec817438582956c04f7dd077839136b8c6e6132
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-07 11:12:20 +0100

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/myprojectweb/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,59 @@
+package com.myprojectweb.course.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.myprojectweb.course.entities.Order;
+import com.myprojectweb.course.entities.Product;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPk other = (OrderItemPk) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+
+}
