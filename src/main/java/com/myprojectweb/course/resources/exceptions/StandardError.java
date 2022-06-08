27a0cff8c48d0a96099c0be2874bdfc76a1cd545
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-08 22:22:15 +0100

    Exception handling - findById

---
+++ src/main/java/com/myprojectweb/course/resources/exceptions/StandardError.java
@@ -0,0 +1,71 @@
+package com.myprojectweb.course.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timestamp;
+	private Integer status;
+	private String erros;
+	private String message;
+	private String path;
+	
+	public StandardError() {		
+	}
+
+	public StandardError(Instant timestamp, Integer status, String erros, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.erros = erros;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getErros() {
+		return erros;
+	}
+
+	public void setErros(String erros) {
+		this.erros = erros;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
--- src/main/java/com/myprojectweb/course/services/UserService.java
+++ src/main/java/com/myprojectweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.myprojectweb.course.entities.User;
 import com.myprojectweb.course.repositories.UserRepository;
+import com.myprojectweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
